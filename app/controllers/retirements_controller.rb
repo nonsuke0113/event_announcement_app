ada89ffc5d8a5fbc7f9b6cdb228ed27a09d1e1e3
nonsuke0113 <asinal.iip36@icloud.com>
2024-09-26 22:50:03 +0900

    feat: Add retirement function

---
+++ app/controllers/retirements_controller.rb
@@ -0,0 +1,11 @@
+class RetirementsController < ApplicationController
+  def new
+  end
+
+  def create
+    return unless current_user.destroy
+
+    reset_session
+    redirect_to root_path, notice: '退会完了しました'
+  end
+end
